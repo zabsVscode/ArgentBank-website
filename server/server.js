dc4b155922addbcfe246f9360b0a15f67eaa3673
zabsVscode <anthonyguiselin62@gmail.com>
2024-05-08 17:10:17 +0200

    ArgentBank-website

--- server/server.js
+++ server/server.js
@@ -8,6 +8,30 @@ const dbConnection = require('./database/connection')
 
 dotEnv.config()
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DATABASE_URL;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -36,3 +60,5 @@ app.get('/', (req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
 })
+
+
