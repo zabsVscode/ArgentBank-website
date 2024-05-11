82810423a9a2b2c5049f299883841e04de32f0ec
zabsVscode <anthonyguiselin62@gmail.com>
2024-05-11 23:48:21 +0200

    React/npm install

---
+++ my-app/src/Signin.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+
+function Signin() {
+  return (
+    <div>
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
+          <img
+            className="main-nav-logo-image"
+            src="./img/argentBankLogo.png"
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
+        </a>
+        <div>
+          <a className="main-nav-item" href="./sign-in.html">
+            <i className="fa fa-user-circle"></i>
+            Sign In
+          </a>
+        </div>
+      </nav>
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div className="input-remember">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            <a href="./user.html" className="sign-in-button">Sign In</a>
+          </form>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Signin;
