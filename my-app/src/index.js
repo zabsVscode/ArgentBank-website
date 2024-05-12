04676740d9fe9311e495e321c09b0d471c9dce92
zabsVscode <anthonyguiselin62@gmail.com>
2024-05-12 20:29:21 +0200

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Assurez-vous d'importer 'Routes'
 import './index.css';
 import App from './App';
 import Signin from './Signin';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
+// Utilisez createRoot de la même manière que vous l'avez fait auparavant
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Route exact path="/" component={App} />
-      <Route path="/signin" component={Signin} />
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
