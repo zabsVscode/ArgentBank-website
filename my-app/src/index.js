995cd6061d1b6a36b69c50c902e6426500fb243a
zabsVscode <anthonyguiselin62@gmail.com>
2024-05-12 14:48:55 +0200

    reactDOM/signin.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Signin from './Signin';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Route exact path="/" component={App} />
+      <Route path="/signin" component={Signin} />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
