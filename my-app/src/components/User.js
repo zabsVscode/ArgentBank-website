dc165bc97937355affb933dce2e4c4a80fcacba9
zabsVscode <anthonyguiselin62@gmail.com>
2024-05-15 18:06:16 +0200

    Components User, package babel, package font-awesome

---
+++ my-app/src/components/User.js
@@ -0,0 +1,71 @@
+// src/User.js
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+import argentBankLogo from './img/argentBankLogo.png';
+
+const User = () => {
+  return (
+    <div>
+      <nav className="main-nav">
+        <Link className="main-nav-logo" to="/">
+          <img className="main-nav-logo-image" src={argentBankLogo} alt="Argent Bank Logo" />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+        <div>
+          <Link className="main-nav-item" to="/user">
+            <i className="fa fa-user-circle"></i>
+            Tony
+          </Link>
+          <Link className="main-nav-item" to="/">
+            <i className="fa fa-sign-out"></i>
+            Sign Out
+          </Link>
+        </div>
+      </nav>
+      <main className="main bg-dark">
+        <div className="header">
+          <h1>Welcome back<br />Tony Jarvis!</h1>
+          <button className="edit-button">Edit Name</button>
+        </div>
+        <h2 className="sr-only">Accounts</h2>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+            <p className="account-amount">$2,082.79</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+            <p className="account-amount">$10,928.42</p>
+            <p className="account-amount-description">Available Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+        <section className="account">
+          <div className="account-content-wrapper">
+            <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+            <p className="account-amount">$184.30</p>
+            <p className="account-amount-description">Current Balance</p>
+          </div>
+          <div className="account-content-wrapper cta">
+            <button className="transaction-button">View transactions</button>
+          </div>
+        </section>
+      </main>
+      <footer className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
+    </div>
+  );
+};
+
+export default User;
